72381c6def0fa623027b02df625c1f8f14ece60e
Max <max@max.local>
2020-02-27 00:23:02 +0300

    Implement feedback system and basic knowledgebase inference

--- src/app/knowledge_base.py
+++ src/app/knowledge_base.py
@@ -1,3 +1,7 @@
+import sqlite3
+from contextlib import closing
+
+
 # Knowledge base mock. Here will be decision model
 class KnowledgeBase:
     def __init__(self):
@@ -9,13 +13,135 @@ class KnowledgeBase:
             'broken packages.': 'Check package dependencies or try to reinstall package',
         }
 
+        self.db_name = 'test.db'
+        conn = sqlite3.connect(self.db_name)
+        self.create_db(conn)
+
+    @staticmethod
+    def create_db(conn):
+        cursor = conn.cursor()
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS problem ('
+            '   problem_id INTEGER PRIMARY KEY,'
+            '   description TEXT NOT NULL)'
+        )
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS recommendation ('
+            '   recommendation_id INTEGER PRIMARY KEY,'
+            '   recommendation TEXT NOT NULL)'
+        )
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS problem_recommendation ('
+            '   problem_id INTEGER,'
+            '   recommendation_id INTEGER,'
+            '   rating INTEGER NOT NULL DEFAULT 0,'
+            '   PRIMARY KEY (problem_id, recommendation_id),'
+            '   FOREIGN KEY (problem_id)'
+            '       REFERENCES problem (problem_id)'
+            '           ON DELETE CASCADE ON UPDATE CASCADE'
+            '   FOREIGN KEY (recommendation_id)'
+            '       REFERENCES recommendation (recommendation_id)'
+            '           ON DELETE CASCADE ON UPDATE CASCADE)'
+        )
+        cursor.close()
+
     def infer(self, problem_description):
-        for key, value in self.data.items():
-            if key in problem_description:
-                return value
+        with sqlite3.connect(self.db_name) as conn:
+            with closing(conn.cursor()) as cursor:
+                # Get problem id or add it to db
+                get_problem_query = 'SELECT problem_id FROM problem WHERE description = ?'
+                problem = cursor.execute(get_problem_query, [problem_description]).fetchone()
 
-        return self.default_recommendation
+                if problem is None:
+                    cursor.execute('INSERT INTO problem VALUES (NULL, ?)', [problem_description])
+                    conn.commit()
+                    problem_id = cursor.lastrowid
+                else:
+                    problem_id = problem[0]
 
-    def complement(self, problem_resume, recommendation):
-        # Replace if exists
-        self.data[problem_resume] = recommendation
+                # Retrieve list of recommendations with rating for specific problem
+                query = f'SELECT recommendation.recommendation_id, recommendation, rating FROM problem' \
+                        f'  INNER JOIN problem_recommendation' \
+                        f'      ON problem.problem_id = problem_recommendation.problem_id' \
+                        f'  INNER JOIN recommendation' \
+                        f'      ON problem_recommendation.recommendation_id = recommendation.recommendation_id' \
+                        f' WHERE problem.problem_id = {problem_id}'
+                recommendations = cursor.execute(query).fetchall()
+
+                # Extend result list with random recommendations if it is too small
+                if len(recommendations) < 5:
+                    amount = 5 - len(recommendations)
+
+                    if amount < 5:
+                        # Ignore already selected recommendations to avoid duplicates
+                        ignore = ', '.join([str(item[0]) for item in recommendations])
+                        query = f'SELECT * FROM recommendation WHERE recommendation_id IN' \
+                                f'   (SELECT recommendation_id FROM recommendation' \
+                                f'      WHERE recommendation_id NOT IN ({ignore})' \
+                                f'      ORDER BY RANDOM() LIMIT {amount})'
+                    else:
+                        query = f'SELECT * FROM recommendation WHERE recommendation_id IN' \
+                                f'   (SELECT recommendation_id FROM recommendation' \
+                                f'      ORDER BY RANDOM() LIMIT {amount})'
+
+                    random_recommendations = cursor.execute(query).fetchall()
+                    random_recommendations = [tuple([0, item[1], 1]) for item in random_recommendations]
+
+                    recommendations.extend(random_recommendations)
+
+                # Sort by rating descending
+                recommendations.sort(key=lambda item: item[2], reverse=True)
+
+        return [r[1] for r in recommendations]
+
+    def rate_recommendation(self, problem, recommendation, did_help):
+        with sqlite3.connect(self.db_name) as conn:
+            with closing(conn.cursor()) as cursor:
+                # Get problem id
+                query = f'SELECT problem_id FROM problem WHERE description = ?'
+                problem_id = cursor.execute(query, [problem]).fetchone()
+
+                if problem_id is None:
+                    return None
+                else:
+                    problem_id = problem_id[0]
+
+                # Get recommendation id
+                query = f'SELECT recommendation_id FROM recommendation WHERE recommendation = ?'
+                recommendation_id = cursor.execute(query, [recommendation]).fetchone()
+
+                if recommendation_id is None:
+                    return None
+                else:
+                    recommendation_id = recommendation_id[0]
+
+                # Increment existing rating or add new
+                query = f'SELECT rating FROM problem_recommendation' \
+                        f'  WHERE problem_id = {problem_id} AND recommendation_id = {recommendation_id}'
+                existing_rating = cursor.execute(query).fetchone()
+
+                if existing_rating is None and did_help:
+                    query = f'INSERT INTO problem_recommendation VALUES' \
+                            f'  ({problem_id}, {recommendation_id}, 1)'
+                    cursor.execute(query)
+                else:
+                    delta = 1 if did_help else -1
+                    query = f'UPDATE problem_recommendation SET rating = {existing_rating[0] + delta}' \
+                            f'  WHERE problem_id = {problem_id} AND recommendation_id = {recommendation_id}'
+                    cursor.execute(query)
+
+                conn.commit()
+
+    def add_problem(self, problem):
+        with sqlite3.connect(self.db_name) as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute('INSERT INTO problem VALUES (NULL, ?)', [problem])
+
+            conn.commit()
+
+    def add_recommendation(self, recommendation):
+        with sqlite3.connect(self.db_name) as conn:
+            with closing(conn.cursor()) as cursor:
+                cursor.execute('INSERT INTO recommendation VALUES (NULL, ?)', [recommendation])
+
+            conn.commit()
--- src/app/routes.py
+++ src/app/routes.py
@@ -4,6 +4,14 @@ from flask_restplus import Resource, fields
 
 problem_description_model = ns.model('ProblemDescription', {'problemDescription': fields.String})
 recommendation_model = ns.model('Recommendation', {'recommendation': fields.String})
+feedback_model = ns.model(
+    'Feedback',
+    {
+        'problemDescription': fields.String,
+        'recommendation': fields.String,
+        'didHelp': fields.Boolean
+    }
+)
 
 complement_input_model = ns.model(
     'ProblemRecommendation',
@@ -14,21 +22,42 @@ complement_input_model = ns.model(
 )
 
 
-@ns.route('/GetRecommendation')
+@ns.route('/GetRecommendations')
 class GetRecommendation(Resource):
     @ns.doc(body=problem_description_model, required=True)
     @ns.response(200, 'Success', recommendation_model)
     def post(self):
-        recommendation = kb.infer(ns.payload['problemDescription'])
-        return {'recommendation': recommendation}
+        recommendations = kb.infer(ns.payload['problemDescription'])
+        return [{'recommendation': r} for r in recommendations]
 
 
-@ns.route('/ComplementKnowledgeBase')
-class ComplementKnowledgeBase(Resource):
-    @ns.doc(body=complement_input_model, required=True)
+@ns.route('/LeaveFeedback')
+class LeaveFeedback(Resource):
+    @ns.doc(body=feedback_model, required=True)
     @ns.response(204, 'Success', None)
     def post(self):
         data = ns.payload
-        kb.complement(data['problemResume'], data['recommendation'])
+        kb.rate_recommendation(
+            data['problemDescription'],
+            data['recommendation'],
+            data['didHelp'])
 
+        return '', 204
+
+
+# @ns.route('/AddProblem')
+# class AddProblem(Resource):
+#     @ns.doc(body=problem_description_model, required=True)
+#     @ns.response(204, 'Success', None)
+#     def post(self):
+#         kb.add_problem(ns.payload['problemDescription'])
+#         return '', 204
+
+
+@ns.route('/AddRecommendation')
+class AddRecommendation(Resource):
+    @ns.doc(body=recommendation_model, required=True)
+    @ns.response(204, 'Success', None)
+    def post(self):
+        kb.add_recommendation(ns.payload['recommendation'])
         return '', 204
